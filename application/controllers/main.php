06bfc21b7bfec5b76043c64e9246f20e791087ed
Ernesto <ernesto.hegi@gmail.com>
2013-11-08 12:22:49 +0100

    Minor changes.

---
+++ application/controllers/main.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+
+        /* Standard Libraries of codeigniter are required */
+        $this->load->database();
+        $this->load->helper('url');
+        /* ------------------ */
+
+        $this->load->library('grocery_CRUD');
+    }
+
+    public function index()
+    {
+    }
+
+    public function page()
+    {
+        $crud = $this->grocery_crud;
+
+        $crud->set_table('page');
+        $crud->set_relation('id_template','template','name');
+
+        $this->load->view('page_crud.php', $crud->render());
+    }
+}
