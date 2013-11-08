ee6597bbf069b1a2b5f87ee43f49c49dd265aca6
Ernesto <ernesto.hegi@gmail.com>
2013-11-08 15:32:13 +0100

    RedBean PHP ORM implemented.

--- application/models/page_m.php
+++ application/models/page_m.php
@@ -4,6 +4,7 @@ class Page_m extends CI_Model {
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->library('rb');
 	}
 
 	function get_all() {
@@ -11,6 +12,12 @@ class Page_m extends CI_Model {
 	}
 
 	function get_by_id($id) {
+		$pages = R::find('page', 'id = ?', array($id));
+
+		echo '<pre>';
+		var_dump($pages);
+		echo '</pre>';
+
 		echo $id;
 	}
 }
