333b06b8c8ac47b660943b32134d86b850785827
Ernesto <ernesto.hegi@gmail.com>
2013-11-08 12:41:20 +0100

    Main template renamed to admin. Page method added. Template table SQL added.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,15 +1,13 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Admin extends CI_Controller {
 
     function __construct()
     {
         parent::__construct();
 
-        /* Standard Libraries of codeigniter are required */
         $this->load->database();
         $this->load->helper('url');
-        /* ------------------ */
 
         $this->load->library('grocery_CRUD');
     }
