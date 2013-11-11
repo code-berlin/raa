68725e7ebb4a16f3352a3415f19640021e5fa191
Ernesto <ernesto.hegi@gmail.com>
2013-11-11 15:06:36 +0100

    RAACMS-23 Minor changes.

--- application/models/type_m.php
+++ application/models/type_m.php
@@ -11,11 +11,4 @@ class Type_m extends CI_Model {
 		return R::findOne('type', 'name = :name',
 			array(':name' => $name));
 	}
-
-	public function save_type($data) {
-		/*$type = R::dispense('type');
-		$type->name = $data['object_name'];
-		$type->slug = $data['object_name'];
-		return R::find('page');*/
-	}
 }
