18385764d17c3def7e1b57e7021cf2ad0a15c413
unknown <rgozdzialski@CODE-B-PC-023.kko.corp>
2013-11-12 10:45:51 +0100

    refactoring controllers/index.php

--- database/user.sql
+++ database/user.sql
@@ -1,19 +1,32 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `mydb` ;
+
 -- -----------------------------------------------------
 -- Table `mydb`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT(4) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+  `id` INT(2) NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(128) NOT NULL,
   `password` VARCHAR(128) NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
 -- Data for table `mydb`.`user`
 -- -----------------------------------------------------
-
-INSERT INTO `user` (`id`, `username`, `password`)
-VALUES (NULL, 'admin@code-b.com', 'b5edbafd026b773a7eb36d9b42b1bd6a952ddfdd');
+START TRANSACTION;
+USE `mydb`;
+INSERT INTO `mydb`.`user` (`id`, `username`, `password`) VALUES (NULL, 'admin@code-b.com', 'b5edbafd026b773a7eb36d9b42b1bd6a952ddfdd');
 
 COMMIT;
 
