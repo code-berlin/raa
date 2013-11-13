18b2e3390b841211656b9b438512244ab5d70e33
kburchardi <kb@jnamic.com>
2013-11-13 15:37:33 +0100

    * added new settings migration

---
+++ application/migrations/006_add_settings.php
@@ -0,0 +1,22 @@
+<?php
+class Migration_Add_Settings extends CI_Migration {
+    public function up() {
+        $this->db->query("CREATE TABLE IF NOT EXISTS `settings` (
+                            `id` int(11) NOT NULL AUTO_INCREMENT,
+                            `blog_title` varchar(255) NOT NULL,
+                            `email` varchar(255) NOT NULL,
+                            `seo` varchar(512) NOT NULL,
+                            `keywords` varchar(512) NOT NULL,
+                              PRIMARY KEY (`id`)
+                           ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ");
+
+
+    }
+
+    public function down() {
+        $this->dbforge->drop_table('settings');
+
+
+    }
+}
+
