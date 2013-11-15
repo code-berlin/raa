c15431b508b5001a7a672673a34e4f9e021bc033
Filippo Benedetti <f.benedetti@code-b.com>
2013-11-15 16:54:49 +0100

    migration 13 type bug fixed

--- application/migrations/013_add_product_table.php
+++ application/migrations/013_add_product_table.php
@@ -14,7 +14,7 @@ class Migration_Add_Product_Table extends CI_Migration {
                         PRIMARY KEY (`id`)
                       ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;');
 
-      $this->db->query('INSERT INTO `type` (`name`) VALUES (`product`);');
+      $this->db->query("INSERT INTO `type` (`id`, `name`) VALUES (4, 'product');");
 
 
 
@@ -22,5 +22,6 @@ class Migration_Add_Product_Table extends CI_Migration {
 
     public function down() {
         $this->dbforge->drop_table('product');
+        $this->db->query("delete from `type` where `id` = 4;");
     }
 }
