45dee4aff5cb5775a5422edc1bbc057c21ebe508
Ernesto <ernesto.hegi@gmail.com>
2014-06-27 09:35:02 +0200

    RAACMS-107: Adding latest changes from old ticket.

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,49 +1,46 @@
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de" xml:lang="de" >
 <head>
+    <title>RAACMS</title>
 
-    <link charset="utf-8" href="<?php echo base_url('/application/css/main.css')?>" rel="stylesheet" type="text/css" />
+    <link charset="utf-8" href="<?php echo base_url('/application/css/admin.css')?>" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url('../assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet" media="screen" />
-	<title>RAA CMS</title>
 </head>
 <body>
+    <div class="login-container">
+        <h1>RAACMS</h1>
 
-    <h1>Login</h1>
+        <?php echo form_open('/auth/login'); ?>
 
-    <?php echo form_open('/auth/login'); ?>
+            <?php echo form_label('Email', 'email'); ?>
+            <?php echo form_input('email', set_value('email'),'id="email"'); ?>
 
-        <?php echo form_label('Username', 'user_name'); ?>
-        <?php echo form_input('user_name', set_value('user_name'),'id="user_name"'); ?>
+            <?php echo form_label('Password', 'user_password'); ?>
+            <?php echo form_password('user_password', '', 'id="user_password"'); ?>
 
-        <?php echo form_label('Password', 'user_password'); ?>
-        <?php echo form_password('user_password', '', 'id="user_password"'); ?>
+            <?php
+                $submit_attributes = array(
+                    'class' => 'btn btn-info',
+                    'value' => 'Login'
+                );
+            ?>
 
-        <?php
+            <div>
+                <?php echo form_submit($submit_attributes, 'submit'); ?>
+            </div>
 
-            $submit_attributes = array(
-            'class' => 'btn btn-default',
-            'value' => 'Login'
-            );
+        <?php echo form_close(); ?>
 
-        ?>
+        <div class="errors" style="color: red;">
+            <?php
+                echo validation_errors();
 
-        <div>
-            <?php echo form_submit($submit_attributes, 'submit'); ?>
+                if (isset($disabled_message) && !empty($disabled_message))
+                {
+                    echo '<div class="alert alert-danger">' . $disabled_message . '</div>';
+                }
+            ?>
         </div>
-
-    <?php echo form_close(); ?>
-
-    <div class="errors" style="color: red;">
-        <?php
-            echo validation_errors();
-
-            if (isset($disabled_message) && !empty($disabled_message))
-            {
-                echo $disabled_message;
-            }
-        ?>
     </div>
-    <script src="<?php echo site_url('assets/bootstrap/js/bootstrap.min.js')?>"></script>
- 
 </body>
 </html>
