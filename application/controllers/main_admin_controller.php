545e19d62bcdedd20d70a5d12794c37fd6079cc1
Ernesto <ernesto.hegi@gmail.com>
2014-07-14 10:53:53 +0200

    TEST: Checks problems accessing the admin panel.

--- application/controllers/main_admin_controller.php
+++ application/controllers/main_admin_controller.php
@@ -36,6 +36,8 @@ class Main_Admin_Controller extends CI_Controller {
     {
         if (!$this->auth_l->user_logged_in())
         {
+            echo 'redirect auth';
+            die;
             redirect('auth');
         }
     }
