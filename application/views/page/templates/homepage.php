299c273de8946aa1132ee327bb40f8f62599d5cf
r-gozdzialski <r.gozdzialski@code-b.com>
2014-11-05 14:51:43 +0100

    - #IP-4 Adds homrpage template file

--- application/views/page/templates/homepage.php
+++ application/views/page/templates/homepage.php
@@ -1,4 +1,5 @@
 <h1>Welcome to RAA CMS</h1>
+
 <ul>
 	<li>Go to <strong><a href="<?php echo base_url('/admin'); ?>">admin panel</a></strong> to get to the backend (superadmin@code-b.com:codeb)</li>
 	<li>Go to <strong>/views/page/templates/homepage.php</strong> to modify this page</li>
