ac9a2274086b436c31f3b3334c655253c0dccdf3
r-gozdzialski <r.gozdzialski@code-b.com>
2014-11-06 10:40:04 +0100

    - #IP-5 Adds migration: facebook_user table

---
+++ application/migrations/044_create_facebook_user_table.php
@@ -0,0 +1,25 @@
+<?php
+require_once(APPPATH . 'migrations/basic_migration.php');
+
+class Migration_Create_Facebook_User_Table extends Basic_migration {
+    function __construct()
+    {
+        parent::__construct();
+        $this->filename =  __FILE__;
+    }
+    public function mig_up() {
+        $this->db->query(
+            "CREATE TABLE IF NOT EXISTS `facebook_user` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `facebook_id` bigint(30) NOT NULL,
+              `name` varchar(255) NOT NULL,
+              `email` varchar(255) NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=65;"
+        );        
+    }
+
+    public function mig_down() {
+        $this->db->query('DROP TABLE `facebook_user`;');
+    }
+}
