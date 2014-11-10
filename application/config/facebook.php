0a58eb54f5f0e941a620cc37bec9bfb8af0e6b7e
r-gozdzialski <r.gozdzialski@code-b.com>
2014-11-10 15:05:59 +0100

    - #IP-5 Removes config data

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,8 +1,8 @@
 <?php
 
-$config['facebook']['app_id'] = '1549286808650603';
-$config['facebook']['app_secret'] = '28bf8e6c1fa34a280a98b8265f1432b4';
-$config['facebook']['app_path'] = 'https://apps.facebook.com/raacmstest';
+$config['facebook']['app_id'] = '';
+$config['facebook']['app_secret'] = '';
+$config['facebook']['app_path'] = ''; // like: https://apps.facebook.com/[name_of_the_app]
 
 // Usually we need one extra permission at the beginning of any project: email.
 // If more permissions are needed, they should be added below as comma-separated values.
