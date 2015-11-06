d9d5a21a8a3ffe54eab24751d0a838ae42e7b388
Robert Bertel <r.bertel@code-b.com>
2015-11-06 17:42:12 +0100

    teaser merge fuck fix

--- application/views/component/teaser.php
+++ application/views/component/teaser.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	if (!empty($teaser)) {
 		foreach ($teaser as $key => $value) {
 
@@ -13,30 +13,7 @@
 			}
 
 			$this->load->view('teaser/teaser_' . $value['type'], $value);
-		
+
 		}
 	}
-?>	
-
-<!--
-<h2 class="home-h2">Tinnitus-Wissen</h2>
-
-<?php $this->load->view('teaser/teaser_main'); ?>
-
-<h2 class="home-h2">Tinnitus-Wissen</h2>
-
-<?php $this->load->view('teaser/teaser_1_big_left_4_small_right'); ?>
-
-<h2 class="home-h2">Ursachen für Tinnitus</h2>
-
-<?php $this->load->view('teaser/teaser_default_dummy'); ?>
-
-<h2 class="home-h2">Symptome und Diagnose</h2>
-
-<?php $this->load->view('teaser/teaser_1_big_top_6_small_bottom'); ?>
-
-<h2 class="home-h2">Tinnitus-Behandlung</h2>
-
-<?php $this->load->view('teaser/teaser_default_dummy'); ?>
-
--->
+?>
