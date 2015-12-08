5180451728073127e3f43cec307248b4d1fd99a0
Marcel Tuch <m.tuch@code-b.com>
2015-12-08 12:07:30 +0100

    - more stuff for theming

--- application/config/migration.php
+++ application/config/migration.php
@@ -3,6 +3,6 @@
 $config['migration_enabled'] = TRUE;
 $config['migration_version'] = 50;
 $config['migration_path'] = APPPATH . 'migrations/';
-$config['migration_version_fork'] = 1;
-$config['migration_path_fork'] = APPPATH . 'migrations_fork/';
+$CI =& get_instance();
+$CI->load->config('migration_fork');
 $config['admin_email'] = 'tech@code-b.com';
