a964c4964fa40e600f9db446b90fc7fb1073ac9f
Robert Bertel <r.bertel@code-b.com>
2016-01-05 17:53:51 +0100

    S24-DOC-594 added searchterm to view

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -25,6 +25,7 @@ class Search extends CI_Controller {
         $this->load->library('theme');
 
         $subdata['lib_data'] = $this->theme->get_template_data();
+        $subdata['searchterm'] = $_REQUEST['gs'];
 
     	$data['template_content'] = $this->load->view('search/gsearch', $subdata, true);
 
