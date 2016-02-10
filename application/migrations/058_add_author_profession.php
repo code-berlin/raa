26b2f1cddd63ed1bf9ab207452f3677ce601f5bc
Marcel Tuch <m.tuch@code-b.com>
2016-02-10 11:50:34 +0100

    - #EX-17 added headline

--- application/migrations/058_add_author_profession.php
+++ application/migrations/058_add_author_profession.php
@@ -8,13 +8,8 @@ class Migration_Add_Author_Profession extends Basic_migration {
         $this->filename =  __FILE__;
     }
     public function mig_up() {
-
-
         $this->db->query('ALTER TABLE `author` CHANGE  `position` `profession` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;');
-
         $this->db->query('ALTER TABLE `author` ADD `position` INT(11) NOT NULL;');
-
-
     }
 
     public function mig_down() {
--- application/views/page/templates/with_sidebar.php
+++ application/views/page/templates/with_sidebar.php
@@ -4,6 +4,8 @@ type: <?php echo $type; ?>
 <br /><br />
 menu_title: <?php echo $page->menu_title; ?>
 <br /><br />
+headline: <?php echo $page->headline; ?>
+<br /><br />
 id: <?php echo $page->id; ?>
 <br /><br />
 text: <?php echo $page->text; ?>
