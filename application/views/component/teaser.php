5e2fd3fe8346656094e5cba2ec5e61d3f290ce18
Robert Bertel <r.bertel@code-b.com>
2016-03-01 18:49:57 +0100

    #EX-28 added jumpmark span if jumpmark is set

--- application/views/component/teaser.php
+++ application/views/component/teaser.php
@@ -2,14 +2,14 @@
 	if (!empty($teaser)) {
 		foreach ($teaser as $key => $value) {
 
-			if (isset($value['title'])) {
+			if (isset($value['jumpmark'])) { ?>
+				<span id="jumpmark_<?php echo $value['jumpmark']; ?>"></span>
+			<?php
+			}
 
-		?>
-
-			<h2 class="teaser-h2"><?php echo $value['title']; ?></h2>
-
-		<?php
-
+			if (isset($value['title'])) { ?>
+				<h2 class="teaser-h2"><?php echo $value['title']; ?></h2>
+			<?php
 			}
 
 			$this->load->view('teaser/teaser_' . $value['teaser_type'], $value);
