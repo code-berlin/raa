63a9ab858c03c0cb596e0379e427e3e439f272a2
Robert Bertel <r.bertel@code-b.com>
2016-07-22 13:55:49 +0200

    themable djproject comp

--- application/views/component/dj_projekt.php
+++ application/views/component/dj_projekt.php
@@ -1,4 +1,4 @@
-<a href="<?php echo base_url('/ueber-uns'); ?>" class="dj-projekt _white flex-container">
+<a href="<?php echo base_url('/ueber-uns'); ?>" class="dj-projekt <?php echo isset($color) ? '_'.$color : ''; ?> flex-container">
     <div class="__left flex">Ein Projekt von DocJones.de</div>
     <div class="__right">
         <div class="__img"></div>
