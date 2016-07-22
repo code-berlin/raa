f42198dcc0bc1db355df12aadc1939dc3e0e7a49
Robert Bertel <r.bertel@code-b.com>
2016-07-22 13:18:17 +0200

    moved djProjekt component to raa

---
+++ application/views/component/dj_projekt.php
@@ -0,0 +1,6 @@
+<a href="<?php echo base_url('/ueber-uns'); ?>" class="dj-projekt _white flex-container">
+    <div class="__left flex">Ein Projekt von DocJones.de</div>
+    <div class="__right">
+        <div class="__img"></div>
+    </div>
+</a>
