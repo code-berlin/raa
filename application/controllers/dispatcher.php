63aa490769005df01c6ac55b4653af43857f83b1
Marcel Tuch <m.tuch@code-b.com>
2017-01-23 17:30:50 +0100

    - fix for check for https

--- application/controllers/dispatcher.php
+++ application/controllers/dispatcher.php
@@ -44,7 +44,7 @@ class Dispatcher extends Page {
 
             $page = $this->page_m->get_by_id($this->settings_m->get_homepage());
 
-            if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] != $this->data['canonical_url']) redirect($this->data['canonical_url']);
+            if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] != $this->data['canonical_url']) redirect($this->data['canonical_url']);
 
             // If $page is false, it means there's no homepage set yet
             if (!$page) {
