a9d46ba6302a763f07c4a64f9919f3788abb0be9
Robert Bertel <r.bertel@code-b.com>
2017-04-03 13:41:20 +0200

    EMRT-48 get menu_title_mobile from db via helper

--- application/helpers/menu_helper.php
+++ application/helpers/menu_helper.php
@@ -22,6 +22,7 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
             foreach ($submenu as $k => $v) {
                 $data['menu'][$value['name']][$k]['slug'] = $v['slug'];
                 $data['menu'][$value['name']][$k]['menu_title'] = $v['menu_title'];
+                $data['menu'][$value['name']][$k]['menu_title_mobile'] = $v['menu_title_mobile'];
                 $data['menu'][$value['name']][$k]['parent_slug'] = $v['parent_slug'];
                 $data['menu'][$value['name']][$k]['children'] = $ci->menu_item_m->get_menu_items_by_menu_id_and_parent_id($value['id'], $v['id']);
                 $data['menu'][$value['name']][$k]['jumpmark'] = $v['jumpmark'];
