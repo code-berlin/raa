69e19eca36dd3755658ebca71a9a5069a4509ebd
Robert Bertel <r.bertel@code-b.com>
2017-04-24 11:28:57 +0200

    EDT-29 add image_and_dropdown_switch teaser

--- application/config/migration.php
+++ application/config/migration.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['migration_enabled'] = TRUE;
-$config['migration_version'] = 90;
+$config['migration_version'] = 91;
 $config['migration_path'] = APPPATH . 'migrations/';
 $CI =& get_instance();
 $CI->load->config('migration_fork');
