334524383a5f0df86542e8026f9d83b6db606dc9
Robert Bertel <r.bertel@code-b.com>
2017-04-24 15:13:28 +0200

    EDT-29 teaser template missing div fixed

--- application/views/component/teaser.php
+++ application/views/component/teaser.php
@@ -3,72 +3,85 @@
 <?php
 	if (!empty($teaser)) {
 
-		$row = false;
+		$i = 0;
+		$teaserCopy = $teaser;
 
 		foreach ($teaser as $key => $value) {
 
 			if (is_file(APPPATH . 'views/teaser/teaser_' . $value['teaser_type'] . EXT) || is_file(APPPATH . 'views/themes/'. $theme . '/teaser/teaser_' . $value['teaser_type'] . EXT)) { ?>
 				
 				<?php
-				// no column teaser next, no row is open -> open teaser group
-				if(!$value['is_column'] && !$row) { ?>
-					<div class="teaser-group">
+				// GET PREV TEASER INSTANCE
+				$_prev = false;
+				if ($i > 0) {
+					$_prev = prev($teaserCopy);
+					next($teaserCopy);
+				}
+
+				// GET NEXT TEASER INSTANCE
+				$_next = false;
+				if ($i < (count($teaserCopy)-1)) {
+					$_next = next($teaserCopy);
+					prev($teaserCopy);
+				}
+
+				// OPEN GROUP IF ACTUAL TEASER IS NOT A COLUMN
+				if(!$value['is_column']) { ?>
+						<div class="teaser-group"> <?php
+				} else {
+					// OPEN ROW IF ACTUAL TEASER IS A COLUMN AND PREVIOUS NOT
+					if(!$_prev['is_column']) { ?>
+						<div class="teaser-group">
+						<div class="flex-container __row"> <?php
+					} 
+						// OPEN COL IF ACTUAL TEASER IS A COLUMN ?>
+						<div class="flex __col"> <?php
+				} ?>
+
+						<?php
+						// RENDER TEASER CONTENT
+
+						if (isset($value['jumpmark'])) { ?>
+							<a name="jumpmark_<?php echo $value['jumpmark']; ?>"></a>
+						<?php
+						}
+
+						if (isset($value['title'])) { ?>
+							<h2 class="teaser-h2"><?php echo $value['title']; ?></h2>
+						<?php
+						}
+
+						if (isset($value['text'])) { ?>
+							<h3 class="teaser-h3"><?php echo $value['text']; ?></h3>
+						<?php
+						}
+
+						$this->load->view('teaser/teaser_' . $value['teaser_type'], $value); 
+
+						?>
+							
+						</div>
 				
 				<?php
-				// no column teaser next, row is open -> close row - close teaser group  - open teaser group
-				} else if(!$value['is_column'] && $row) {
-					$row = false; ?>
-					</div>
-					</div>
-					<div class="teaser-group">
-				
-				<?php
-				// column teaser next, no row is open -> open teaser group - open row - open column
-				} else if($value['is_column'] && !$row) {
-					$row = true; ?>
-					<div class="teaser-group">
-					<div class="flex-container __row">
-					<div class="flex __col">
-				
-				<?php
-				// column teaser next, row is open -> open column
-				} else if($value['is_column'] && $row) { ?>
-					<div class="flex __col">
+				// CLOSE ROW IF ACTUAL TEASER IS COLUMN AND NEXT NOT
+				if($value['is_column'] && !$_next['is_column']) { ?>
+						</div>
+						</div>
 				<?php
 				} ?>
-						
-					<?php
-					// TEASER RELATED START
-
-					if (isset($value['jumpmark'])) { ?>
-						<a name="jumpmark_<?php echo $value['jumpmark']; ?>"></a>
-					<?php
-					}
-
-					if (isset($value['title'])) { ?>
-						<h2 class="teaser-h2"><?php echo $value['title']; ?></h2>
-					<?php
-					}
-
-					if (isset($value['text'])) { ?>
-						<h3 class="teaser-h3"><?php echo $value['text']; ?></h3>
-					<?php
-					}
-
-					$this->load->view('teaser/teaser_' . $value['teaser_type'], $value); 
-
-					// TEASER RELATED END 
-					?>
-
-				</div>
+					
 
 			<?php
 			} else {
 				echo "Teaser Type '" . $value['teaser_type'] . "' is not available for this theme. Please choose another in your admin.</br>";
 			}
 
-		}
-	}
+			$i++;
+			next($teaserCopy);
+
+		} // foreach
+
+	} // if 
 ?>
 
 </div>
