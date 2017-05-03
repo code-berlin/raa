e3cb878c2aab1fa3a9818dd8c0a2b3a6574ea371
Marcel Tuch <m.tuch@code-b.com>
2017-05-03 13:42:16 +0200

    - added config use

--- application/helpers/price_comparison_api_helper.php
+++ application/helpers/price_comparison_api_helper.php
@@ -3,11 +3,16 @@
 
 function get_drug_info($pzn) {
 
-	// http://cis3.docjones.de/customer_information/pzn/4/1/1/09461151
-	$base = 'http://cis3.docjones.de/customer_information/pzn/';
-	$ad_type = 4;
-	$ad_subtype = 1;
-	$portal_id = 1;
+    $CI = & get_instance();
+    $CI->config->load('price_comparison');
+
+    $api_config = $CI->config->item('price_comparison');
+
+	// http://cis3.docjones.de/customer_information/pzn/4/0/1/00172333
+	$base = $api_config['base'];
+	$ad_type = $api_config['ad_type'];
+	$ad_subtype = $api_config['ad_subtype'];
+	$portal_id = $api_config['portal_id'];
 
 
 	$ch =  curl_init($base . $ad_type . '/' . $ad_subtype . '/' . $portal_id . '/' . $pzn);
