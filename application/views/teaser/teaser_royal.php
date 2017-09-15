4c07d88b160140b010fc800f4203ac33b2d7c515
Robert Bertel <r.bertel@code-b.com>
2017-09-15 09:57:18 +0200

    EX-65 teaserRoyal anchor improvemnts

--- application/views/teaser/teaser_royal.php
+++ application/views/teaser/teaser_royal.php
@@ -1,60 +1,39 @@
 <?php
-foreach ($items as $key => $value) {
+foreach ($items as $key => $value) {  ?>
 
-    $target = '_self';
-    $image = null;
-    $slug = null;
+    <div class="teaser-royal <?php echo !empty($value['slug']) ? 'js-teaser-linked' : ''; ?>">
 
-    if ($value['content_type'] == 'external') {
-        $target = '_blank';
-    }
-
-    if (isset($value['external_image'])) {
-        $image = $value['external_image'];
-    } else {
-        $image = $value['page_image'];
-    }
-
-    if (isset($value['external_link'])) {
-        $slug = $value['external_link'];
-    } else if (isset($value['page_slug'])) {
-        $slug = base_url((isset($value['parent_slug']) && !empty($value['parent_slug']) ? $value['parent_slug'] . '/' : '') . $value['page_slug']);
-    } ?>
-
-    <?php
-    if (isset($slug)) { ?>
-        <a href="<?php echo $slug; ?>" target="<?php echo $target; ?>" class="teaser-royal">
-    <?php
-    } else { ?>
-        <div class="teaser-royal">
-    <?php
-    } ?>
-
-            <img class="lazy-img js-lazy-img" src="<?php echo $img_placeholder; ?>" data-src="<?php echo isset($image) ? $image : ''; ?>" alt="<?php echo $value['title']; ?>">
-            <div class="__info">
-                <h1><?php echo $value['title']; ?></h1>
-                <div class="__text">
-                    <?php echo $value['text']; ?>
-                </div>
-
+        <img
+            class="lazy-img js-lazy-img"
+            src="<?php echo $img_placeholder; ?>"
+            data-src="<?php echo isset($value['image']) ? $value['image'] : ''; ?>"
+            alt="<?php echo $value['title']; ?>"
+        >
+        <div class="__info">
+            <h1>
                 <?php
-                if (isset($slug)) { ?>
-                    <div class="__link-wrap">
-                        <div class="__link">Weiterlesen</div>
-                    </div>
-                <?php
+                if (!empty($value['slug'])) {
+                    $this->load->view('component/link',
+                        array('href' => $value['slug'], 'target' => $value['target'], 'text' => $value['title']));
+                } else {
+                    echo $value['title'];
                 } ?>
-
+            </h1>
+            <div class="__text">
+                <?php echo $value['text']; ?>
             </div>
 
-    <?php
-    if (isset($slug)) { ?>
-        </a>
-    <?php
-    } else { ?>
+            <?php
+            if (!empty($value['slug'])) { ?>
+                <div class="__link-wrap">
+                    <div class="__link">Weiterlesen</div>
+                </div>
+            <?php
+            } ?>
+
         </div>
-    <?php
-    } ?>
+
+    </div>
 
 <?php
 }
