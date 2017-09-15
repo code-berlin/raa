8a8177d66c078e1a7f5b0244f89b6dde79e9105c
Robert Bertel <r.bertel@code-b.com>
2017-09-15 10:07:23 +0200

    EX-65 teaserMain anchor improvemnts

--- application/views/teaser/teaser_main.php
+++ application/views/teaser/teaser_main.php
@@ -1,42 +1,34 @@
 <?php
-    foreach ($items as $key => $value) {
+    foreach ($items as $key => $value) { ?>
 
-        $target = '_self';
-        $image = null;
-        $slug = null;
-
-        if ($value['content_type'] == 'external') {
-            $target = '_blank';
-        }
-
-        if (isset($value['external_image'])) {
-            $image = $value['external_image'];
-        } else {
-            $image = $value['page_image'];
-        }
-
-        if (isset($value['external_link'])) {
-            $slug = $value['external_link'];
-        } else if (isset($value['page_slug'])) {
-            $slug = base_url((isset($value['parent_slug']) && !empty($value['parent_slug']) ? $value['parent_slug'] . '/' : '') . $value['page_slug']);
-        } ?>
-
-        <div class="teaser-main flex-container">
+        <div class="teaser-main flex-container <?php echo !empty($value['slug']) ? 'js-teaser-linked' : ''; ?>">
             <div class="__headline flex">
-                <h1><?php echo $value['title']; ?></h1>
+                <h1>
+                    <?php
+                    if (!empty($value['slug'])) {
+                        $this->load->view('component/link',
+                            array('href' => $value['slug'], 'target' => $value['target'], 'text' => $value['title']));
+                    } else {
+                        echo $value['title'];
+                    } ?>
+                </h1>
                 <div class="__sub">
                     <?php echo $value['text']; ?>
                 </div>
             </div>
             <div class="__img">
-                <img class="lazy-img js-lazy-img" src="<?php echo $img_placeholder; ?>" data-src="<?php echo isset($image) ? $image : ''; ?>" alt="<?php echo $value['title']; ?>">
+                <img
+                    class="lazy-img js-lazy-img"
+                    src="<?php echo $img_placeholder; ?>"
+                    data-src="<?php echo !empty($value['image']) ? $value['image'] : ''; ?>"
+                    alt="<?php echo $value['title']; ?>">
 
                 <?php
-                if (isset($slug)) { ?>
+                if (!empty($value['slug'])) { ?>
                     <div class="__link-wrap">
-                        <a class="__link" href="<?php echo $slug; ?>" target="<?php echo $target; ?>">
+                        <div class="__link">
                             Weiterlesen<i class="fa fa-chevron-right"></i>
-                        </a>
+                        </div>
                     </div>
                 <?php
                 } ?>
