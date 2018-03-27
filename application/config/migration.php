b739ca5610d8d970f30edeb3740994104d934cac
Marcel Tuch <m.tuch@code-b.com>
2018-03-27 14:27:33 +0200

    - #EX-73 removed nativendo

--- application/config/migration.php
+++ application/config/migration.php
@@ -1,8 +1,8 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['migration_enabled'] = TRUE;
-$config['migration_version'] = 104;
+$config['migration_enabled'] = true;
+$config['migration_version'] = 105;
 $config['migration_path'] = APPPATH . 'migrations/';
-$CI =& get_instance();
+$CI = &get_instance();
 $CI->load->config('migration_fork');
 $config['admin_email'] = 'tech@code-b.com';
---
+++ application/migrations/105_remove_article_page_nativendo.php
@@ -0,0 +1,23 @@
+<?php
+require_once(APPPATH . 'migrations/basic_migration.php');
+
+class Migration_Add_Article_Page_Nativendo extends Basic_migration
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->filename = __FILE__;
+    }
+
+    public function mig_up()
+    {
+        $this->db->query("DELETE FROM `template` WHERE (`name`) = ('article_page_nativendo');");
+    }
+
+    public function mig_down()
+    {
+        $this->db->query("INSERT INTO `template` (`name`) VALUES ('article_page_nativendo');");
+    }
+
+}
